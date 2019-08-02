5279c0afc5c5c3a5867d2adf1a89f71d74592279
Wesley Fung <fungw@tcd.ie>
2019-08-02 13:12:40 +0100

    Author, description and title tags update, removal of non-used files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `wesleyfung.com`,
+    description: `Source code for personal website wesleyfung.com`,
+    author: `@fungw`,
   },
   plugins: [
     `gatsby-plugin-lodash`,
@@ -15,7 +15,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`
       },
     },
     {
